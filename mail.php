6f9e97abaa30ba36389a7bba54042ea0c047e4bc
venuprasad naik <vpnaik97@gmail.com>
2019-06-03 15:34:02 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -4,6 +4,10 @@
 		The Send Mail php Script for Contact Form
 		Server-side data validation is also added for good data validation.
 	*/
+
+	$url = 'https://api.sendgrid.com/';
+ 	$user = 'azure_38f4e2680c101092816fa7614446d293@azure.com';
+ 	$pass = 'Csjatre@3';
 	
 	$data['error'] = false;
 	
@@ -21,6 +25,37 @@
 	}else if( empty($message) ){
 		$data['error'] = 'The message field is required!';
 	}else{
+		$params = array(
+            'api_user' => $user,
+            'api_key' => $pass,
+            'to' => 'vpnaik97@gmail.com',
+            'subject' => 'testing from curl',
+            'html' => 'testing body',
+            'text' => 'testing body',
+            'from' => 'anna@contoso.com',
+        );
+
+        $request = $url.'api/mail.send.json';
+
+        // Generate curl request
+        $session = curl_init($request);
+
+        // Tell curl to use HTTP POST
+        curl_setopt ($session, CURLOPT_POST, true);
+
+        // Tell curl that this is the body of the POST
+        curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+
+        // Tell curl not to return headers, but do return the response
+        curl_setopt($session, CURLOPT_HEADER, false);
+        curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+
+        // obtain response
+        $response = curl_exec($session);
+        curl_close($session);
+
+        // print everything out
+        print_r($response);
 		
 		$formcontent="From: $name\nSubject: $subject\nEmail: $email\nMessage: $message";
 		
